4aad1a0988f9005b50c3b2622ea3b308dd364445
TTania <116368430+TTania-1@users.noreply.github.com>
2022-11-27 15:17:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("System Failure Imminent!")
+print("System reboot has been initiated...")
+print("                                   ")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print("                                   ")
+print("System online.")
+
+#To display a multi-line message we have to use print function. Morover we can use print function to display a longer multi-line message. Also we use print function for blanked line. It means we can use the print function more than once in our code.
